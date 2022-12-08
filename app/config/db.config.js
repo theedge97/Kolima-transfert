5ef95cdd214590281182beb413c4980f4e1412d5
theedge97 <105657006+theedge97@users.noreply.github.com>
2022-12-08 11:23:39 +0000

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "kolimatransfert"
+  HOST: "31.220.53.233:3306",
+  USER: "sakina",
+  PASSWORD: "Rabiatou+25",
+  DB: "kolimaepargne"
 };
